6a28fb12ec452a88744065f45dd8dc965960ea12
yahyaboyali <yahyaboyali@gmail.com>
2022-10-23 15:56:45 +0300

    classlar eklendi

---
+++ src/entities/Product.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class Product {
+    public int id;
+    public String name;
+    public String description;
+    public double price;
+}
---
+++ src/manager/ProductManager.java
@@ -0,0 +1,9 @@
+package manager;
+
+import entities.Product;
+
+public class ProductManager {
+    public void add(Product p) {
+        System.out.println(p.name +" eklendi");
+    }
+}
